e9146594930a7eea0ec7b89b4108fca7f55abb3e
DilawarDilsher <71361827+DilawarDilsher@users.noreply.github.com>
2021-02-15 15:37:18 -0800

    Add files via upload

--- Food.js
+++ Food.js
@@ -1,43 +1,43 @@
 class Food {
     constructor(){
- var foodStock, lastFed;
- var getFoodStock, updateFoodStock, deductFoodstock ;
-        
+    this.foodStock=0;
+    this.lastFed;
+    this.image=loadImage('Images/Milk.png');
     }
 
-    preload(){
-        milk_Img=loadImage("images/Milk.png");
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
+    }
+
+    getFoodStock(){
+      return this.foodStock;
     }
 
     display(){
-        feed=createButton("Feed The Good Doggy");
-        feed.position(700,95);
-        feed.mousePressed(feedDog);
-
-        addFood=createButton("Add Food");
-        addFood.position(800,95);
-        addFood.mousePressed(addFoods);
-        
-        fill(255,255,254);
-        textSize(15);
-        if(lastFed>=12){
-            text("Last Feed:"+lastfeed%12+"pm",350,30);
-        }else if(lastFed==0){
-            text("Last Fed:12 AM",350,30);
-        }else{
-            text("Last Feed:"+ lastFed + "AM",350,30);
+      var x=80,y=100;
+      
+      imageMode(CENTER);
+      image(this.image,720,220,70,70);
+      
+      if(this.foodStock!=0){
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
         }
-        
+      }
     }
-
-         feedDog(){
-       feedDog.addImage(dogImg1);
-       
-       foodObl.updateFoodStock(foodObj.getFoodStock()-1);
-       database.ref('/').update({
-           Food:foodObj.getFoodStock(),
-           FeedTime:hour()
-       })
-    }
-
 }
